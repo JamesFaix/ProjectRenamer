8101b69c9eccc0eb7c93216eca2828eb87ff8d1e
James Faix <jamesthefaix@gmail.com>
2018-04-18 12:50:03 -0400

    Add project files.

---
+++ ProjectRenamer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectRenamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
